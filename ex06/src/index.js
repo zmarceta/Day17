21f62ff456301c7ef3a644cf0d483458c424ea31
zmarceta <zmarceta83@gmail.com>
2021-03-25 09:02:40 +0100

    Modiifcation 1

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -3,8 +3,7 @@ const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function removeFirstTwo(list) {
 	"use strict";
 	// Only change code below this line
-    const [a, b, ...arr] = list;
-	const array2 = arr;
+    const [,, ...array2] = list;	
 	// Only change code above this line
 	return array2;
 }
